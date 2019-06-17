c2bc5d538633c6547db805bc39cd3eba407676c1
simondowa <simondriesslein@web.de>
2019-06-17 16:06:05 +0200

    Fertigstellung Separate Verkettung

--- AlgoDat/AlgoDat/HashTabSepChain.cs
+++ AlgoDat/AlgoDat/HashTabSepChain.cs
@@ -6,7 +6,164 @@ using System.Threading.Tasks;
 
 namespace AlgoDat
 {
-    class HashTabSepChain : MultiSetUnsortedLinkedList
+    class HashTabSepChain :ISet
     {
+        class HashNode
+        {
+            int key;
+            string data;
+            HashNode next;
+            public HashNode(int key, string data)
+            {
+                this.key = key;
+                this.data = data;
+                next = null;
+            }
+            public int GetKey()
+            {
+                return key;
+            }
+            public string GetData()
+            {
+                return data;
+            }
+            public void SetNextNode(HashNode obj)
+            {
+                next = obj;
+            }
+            public HashNode GetNextNode()
+            {
+                return this.next;
+            }
+        }
+
+        HashNode[] table;
+        const int size = 10;
+
+        public HashTabSepChain()
+        {
+            table = new HashNode[size];
+            for (int i = 0; i < table.Length; i++)
+            {
+                table[i] = null;
+            }
+        }
+        public bool Insert(int key)
+        {
+            if (Search_Help(key))
+            {
+                Console.WriteLine("Wert existiert schon!");
+                return false;
+            }
+
+            HashNode nObj = new HashNode(key, null);
+            int hash = Hash(key);
+            if (table[hash] != null)
+            {
+                nObj.SetNextNode(table[hash]);
+            }
+            table[hash] = nObj;
+            Console.WriteLine("Wert wurde eingetragen!");
+            return true;
+        }
+        public bool Search(int key)
+        {
+            int hash = Hash(key);
+            HashNode current = table[hash];
+            if (current == null)
+            {
+                Console.WriteLine("Wert wurde nicht gefunden!");
+                return false;
+            }
+            while (current.GetKey() != key && current.GetNextNode() != null)
+            {
+                current = current.GetNextNode();
+            }
+            if (current.GetKey() == key)
+            {
+                Console.WriteLine("Wert wurde gefunden!");
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+            
+        }
+        public bool Search_Help(int key)
+        {
+            int hash = Hash(key);
+            HashNode current = table[hash];
+            if (current == null)
+            {
+                return false;
+            }
+            while (current.GetKey() != key && current.GetNextNode() != null)
+            {
+                current = current.GetNextNode();
+            }
+            return (current.GetKey() == key);
+          
+
+        }
+        public bool Delete(int key)
+        {
+            if (!Search(key))
+                return false;
+
+            int hash = Hash(key);
+            bool isRemoved = false;
+
+            if (table[hash].GetKey() == key)
+            {
+                table[hash] = table[hash].GetNextNode();
+                isRemoved = true;
+            }
+            else
+            {
+                HashNode current = table[hash];
+                while (current != null)
+                {
+                    if (current.GetNextNode() != null)
+                    {
+                        if (current.GetNextNode().GetKey() == key)
+                        {
+                            HashNode newNext = current.GetNextNode().GetNextNode();
+                            current.SetNextNode(newNext);
+                            isRemoved = true;
+                            break;
+                        }
+                        else
+                        {
+                            current = current.GetNextNode();
+                        }
+                    }
+
+                }
+            }
+            if (!isRemoved)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void Print()
+        {
+            HashNode current = null;
+            for (int i = 0; i < table.Length; i++)
+            {
+                current = table[i];
+                while (current != null)
+                {
+                    Console.Write(current.GetKey() + " ");
+                    current = current.GetNextNode();
+                }
+                Console.WriteLine();
+            }
+        }
+        private int Hash(int value)
+        {
+            return value % size;
+        }
     }
 }
